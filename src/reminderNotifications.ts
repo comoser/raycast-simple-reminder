0409926d6337fae20b7ccc1577dd809fa797841a
comoser <david.alecrim1@gmail.com>
2023-04-19 16:30:55 +0100

    chore: add todo for baackground task

--- src/reminderNotifications.ts
+++ src/reminderNotifications.ts
@@ -1,7 +1,6 @@
 import { LocalStorage, updateCommandMetadata } from '@raycast/api';
 import { Reminder } from './types/reminder';
 import { runAppleScript } from 'run-applescript';
-import { deleteReminder } from './utils/deleteReminder';
 
 export default async function Command() {
   const storedRemindersObject = await LocalStorage.allItems<Record<string, string>>();
@@ -9,6 +8,7 @@ export default async function Command() {
 
   for (const key in storedRemindersObject) {
     const reminder: Reminder = JSON.parse(storedRemindersObject[key]);
+    // TODO: this date comparison is very crude, needs more refining
     if (new Date(reminder.date).getTime() >= new Date().getTime()) {
       await runAppleScript(
         `display notification "${reminder.topic}" with title "Reminder" sound name "default"`
